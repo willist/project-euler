d0670a2c72be666b2062e076cdec3c6b9c8a5167
Tim Willis <schadenfreude328@gmail.com>
2012-04-10 21:02:11 -0400

    moving utility functions into their own module

--- solutions/0002.py
+++ solutions/0002.py
@@ -1,11 +1,3 @@
-def fib(cap=None):
-    a = 0
-    yield a
-    b = 1
-    while True:
-       yield b
-       a, b = b, a + b
-       if b > cap:
-           break
- 
-print sum(x for x in fib(4000000) if not x % 2)
+from utilities import fibonacci
+
+print sum(x for x in fibonacci(4000000) if not x % 2)
--- solutions/0003.py
+++ solutions/0003.py
@@ -1,29 +1,4 @@
+from utilities import prime_factors
+
 a = 600851475143
-
-import math
-import numpy
-
-def factors(num):
-    sqrt_num = int(math.sqrt(num))
-
-    poss_left_factors = numpy.array([1,2] + list(xrange(3, sqrt_num , 2)))
-
-    facts = []
-    for poss_factor in poss_left_factors:
-        if not num % poss_factor:
-            facts.append(poss_factor)
-            facts.append(num / poss_factor)
-
-    return sorted(facts)
-
-def prime_factors(num):
-    facts = []
-    for item in factors(num):
-        all_factors = factors(item)
-        print item, all_factors
-        if len(all_factors) == 2:
-            facts.append(item)
-
-    return facts
-
 print prime_factors(a)
