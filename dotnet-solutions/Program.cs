503377bcd6ff3514853c18e306b61b1ba8a45643
Tim Willis <schadenfreude328@gmail.com>
2012-04-22 22:12:42 -0400

    translating python solutions to C#.NET

---
+++ dotnet-solutions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler
+{   
+    /// <summary>
+    /// Solutions to various Project Euler problems.
+    /// </summary>
+    /// <seealso cref="http://projecteuler.net"/>
+    public class Solutions
+    {
+        /// <summary>
+        /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+        /// 
+        /// Find the sum of all the multiples of 3 or 5 below 1000.
+        /// </summary>
+        public int Problem001()
+        {
+            return Enumerable.Range(0, 1000).Where(x => (x % 3 == 0 || x % 5 == 0)).Sum();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solutions = new Solutions();
+            Console.WriteLine(String.Format("Answer to Problem 1 = {0}", solutions.Problem001()));
+            Console.ReadKey();
+        }
+    }
+}
