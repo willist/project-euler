0e2e5feb0371c89e461eb9209b1b87d1b10ed4e9
Tim Willis <tim.willis2@coxinc.com>
2014-08-28 13:22:34 -0400

    Add desc and cleanup py3.

--- python-solutions/0003.py
+++ python-solutions/0003.py
@@ -1,4 +1,11 @@
+"""
+Link: https://projecteuler.net/problem=3
+Description:
+    The prime factors of 13195 are 5, 7, 13 and 29.
+
+    What is the largest prime factor of the number 600851475143 ?
+"""
 from utilities import prime_factors
 
-a = 600851475143
-print list(prime_factors(a))
+num = 600851475143
+print max(prime_factors(num))
